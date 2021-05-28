f9621afd3a06ca4b5d3323808cb168d1c0243f52
moinulhossainmahim <moinulhossainmahim@gmail.com>
2021-05-29 02:39:50 +0600

    Added products-details, products html file with css and js file

---
+++ script.js
@@ -0,0 +1,34 @@
+const menuItems = document.querySelector('.menu-items')
+const menuIcon = document.querySelector('.menu-icon')
+const productImage = document.getElementById('product-img')
+const smallImage = document.getElementsByClassName('small-img')
+
+menuItems.style.maxHeight = "0px"
+
+function menuToggle() {
+  if(menuItems.style.maxHeight == "0px") {
+    menuItems.style.maxHeight = "200px"
+  } else {
+    menuItems.style.maxHeight = "0px"
+  }
+}
+
+menuIcon.addEventListener('click', menuToggle)
+
+
+
+smallImage[0].onclick= function() {
+  productImage.src = smallImage[0].src
+}
+
+smallImage[1].onclick= function() {
+  productImage.src = smallImage[1].src
+}
+
+smallImage[2].onclick= function() {
+  productImage.src = smallImage[2].src
+}
+
+smallImage[3].onclick= function() {
+  productImage.src = smallImage[3].src
+}
